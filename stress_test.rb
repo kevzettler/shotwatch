92c0b42278476e2168456921b7189a858d84d772
kevzettler <kevzettler@gmail.com>
2010-09-27 11:22:28 -0700

    updated routine for pulling fresh images of the higest version number

--- stress_test.rb
+++ stress_test.rb
@@ -18,9 +18,9 @@ browsers = [
               :version => "4"}]
 
 # Uncomment to change the picture
-#png_url = "http://blogs.nature.com/news/thegreatbeyond/biohazard.png"
+png_url = "http://blogs.nature.com/news/thegreatbeyond/biohazard.png"
 #png_url = "https://saucelabs.com/jobs/cc166bba20685b34dce43e6b3eae64ab/0001screenshot.png"
-png_url = "http://upload.wikimedia.org/wikipedia/en/0/03/Tarepanda.png"
+#png_url = "http://upload.wikimedia.org/wikipedia/en/0/03/Tarepanda.png"
 
 Parallel.map(browsers, :in_processes => browsers.count)  do |browser|
   url = URI.parse('http://localhost:8081')
